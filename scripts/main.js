41a0f19ce446158816473d18cff75186d692e19d
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2022-02-27 15:15:05 +0000

    Created a grid using 3d Array

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
 //setup onload
-document.onload = setup();
 
-function setup(){
+// var rows = 25
+// var cols = 25
+const table = document.getElementById('grid')
+
+document.onload = setup(25,25);
+
+function setup(cols,rows){
     //calculate amount of cols and rows fit for the screen 
-    console.log("hello sexy banana")
+    for (let i = 0; i < cols; i++) {
+        const tableRow = document.createElement('tr')
+        tableRow.dataset.row = i
+        table.appendChild(tableRow);
+        for (let j = 0; j < rows; j++) {
+            const square = document.createElement('td')
+            square.dataset.position = [i,j], square.dataset.state = "unvisted"
+            if(square.dataset.position == [1,1]) square.dataset.state = "visited"; //refactor this later
+            tableRow.appendChild(square)
+        }
+    }
+}
+
+function RecursiveBackTracking(){
+    
 }
